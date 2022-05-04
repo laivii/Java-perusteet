502dc949df5952b9a7a41f2ab12bc04ef5290887
laivii <91199307+laivii@users.noreply.github.com>
2022-05-04 15:13:36 +0300

    kysynimi

---
+++ KysyNimi.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class KysyNimi {
+    public static void(String[] args){
+        String sukunimi = "";
+        String etunimi = "";
+        Int ika = 0;
+
+        Scanner lukija = new Scanner(System.in);
+
+        System.out.print("Anna sukunimi: ");
+        sukunimi = lukija.nextLine();
+
+        System.out.print("Anna etunimi: ");
+        etunimi = lukija.nextLine();
+
+        System.out.print("Anna ikä: ");
+        ika = lukija.nextInt();
+
+        String kokonimi = etunimi + sukunimi;
+
+        System.out.println("Olet " + kokonimi + "ja ikäsi on " + ika);
+
+        lukija.close();
+    }
+}
